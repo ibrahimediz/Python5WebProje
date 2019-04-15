c1710ccdb5338dd15517309534a0863e3a2f28b3
ibrahimediz <ediz@ediz.com>
2019-04-15 20:14:53 +0300

    Form

--- EdizBlog/urls.py
+++ EdizBlog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.gonderi_list, name='gonder_list'),
+    path('post/<int:pk>/', views.post_detay, name='post_detay'),
 ]
--- EdizBlog/views.py
+++ EdizBlog/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from .models import Gonderi
 
 def gonderi_list(request):
     Gonderiler =  Gonderi.objects.all()
     return render(request, "EdizBlog/liste.html", {'Posts':Gonderiler})
+
+
+def post_detay(request,pk):
+    post = get_object_or_404(Gonderi,pk=pk)
+    return render(request, "EdizBlog/detay.html", {'post':post})
